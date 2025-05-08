72b36d6b1bef7b5315282f66aff3e95b3786d18d
Neha kashyap <nehaneha09276@gmail.com>
2025-05-08 21:25:05 +0530

    Resolved App.jsx merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
 import React from 'react';
-import Navbar from './Components/Navbar/Navbar'
-<<<<<<< HEAD
- import Hero from  './Components/Hero/Hero'
- import About from './Components/About/About'
- import Projects from './Components/Projects/Projects'
- import Contact from './Components/Contact/Contact'
- import Fotter from './Components/Footer/Footer'
-=======
-import Hero from  './Components/Hero/Hero'
-import About from './Components/About/About'
-import Projects from './Components/Projects/Projects'
-import Contact from './Components/Contact/Contact'
-import Fotter from './Components/Footer/Footer'
->>>>>>> d07982507458ed0d1d2a434bd8043ec1202520a4
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import About from './Components/About/About';
+import Projects from './Components/Projects/Projects';
+import Contact from './Components/Contact/Contact';
+import Fotter from './Components/Footer/Footer';
+
 function App() {
   return (
     <div>
-        <Navbar/>
-        <Hero/>
-        <About/>
-        <Projects/>
-        <Contact/>
-<<<<<<< HEAD
-        <Fotter/> 
-=======
-        <Fotter/>
->>>>>>> d07982507458ed0d1d2a434bd8043ec1202520a4
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Contact />
+      <Fotter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,70 +1,34 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-import "./Navbar.css"
-import AnchorLink from'react-anchor-link-smooth-scroll'
-
-function Navbar() {
-const [menu, setMenu] = useState("home");
-=======
 import React, { useState } from 'react';
 import "./Navbar.css";
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 function Navbar() {
   const [menu, setMenu] = useState("home");
->>>>>>> d07982507458ed0d1d2a434bd8043ec1202520a4
 
   return (
     <div className="nav-bar">
       <h3>Portfolio</h3>
-<<<<<<< HEAD
-        <ul className='item'>
-          <li><AnchorLink  href='home' className='anchor-link'><p onClick={()=>{
-          return setMenu("home")
-         }}>Home</p></AnchorLink></li> 
-         
-         <li><AnchorLink href='#about' className='anchor-link'><p onClick={()=>{
-            return setMenu("about")
-         }}>About</p></AnchorLink></li>
-         <li><AnchorLink href='#projects' className='anchor-link'><p onClick={()=>{
-          return setMenu("project")
-         }}>Projcets</p></AnchorLink></li>
-         <li><AnchorLink href='#contact' className='anchor-link'><p onClick={()=>{
-           return setMenu("contact")
-         }}>Contact</p></AnchorLink></li>
-         <button className='connect-nav'>Connect With Me</button>
-       </ul>
-    </div>
-  )
-}
-
-export default Navbar
-=======
       <ul className='item'>
         <li>
           <AnchorLink href='#home' className='anchor-link'>
             <p onClick={() => setMenu("home")}>Home</p>
           </AnchorLink>
         </li>
-
         <li>
           <AnchorLink href='#about' className='anchor-link'>
             <p onClick={() => setMenu("about")}>About</p>
           </AnchorLink>
         </li>
-
         <li>
           <AnchorLink href='#projects' className='anchor-link'>
             <p onClick={() => setMenu("projects")}>Projects</p>
           </AnchorLink>
         </li>
-
         <li>
           <AnchorLink href='#contact' className='anchor-link'>
             <p onClick={() => setMenu("contact")}>Contact</p>
           </AnchorLink>
         </li>
-
         <button className='connect-nav'>Connect With Me</button>
       </ul>
     </div>
@@ -72,4 +36,3 @@ export default Navbar
 }
 
 export default Navbar;
->>>>>>> d07982507458ed0d1d2a434bd8043ec1202520a4
